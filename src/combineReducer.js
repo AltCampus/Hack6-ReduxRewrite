71ddb1b0011188c052edfaac9be467c1c8ff34d7
haripallikere <haripallikere93@gmail.com>
2019-02-13 20:15:38 +0530

    add compose method

--- src/combineReducer.js
+++ src/combineReducer.js
@@ -1,11 +1,11 @@
 export default function combinedReducer(reducers) {
-    const reducersKey = Object.keys(reducers)
-    
-    return function comboReducer(state, action) {
-      reducersKey.forEach(key => {
-        console.log(reducers[key])
-        state[key] = reducers[key](state[key], action)
-      })
-      return state
-    }
-  }
+  const reducersKey = Object.keys(reducers);
+
+  return function comboReducer(state, action) {
+    reducersKey.forEach(key => {
+      console.log(reducers[key]);
+      state[key] = reducers[key](state[key], action);
+    });
+    return state;
+  };
+}
